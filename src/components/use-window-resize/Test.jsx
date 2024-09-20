bc30e1ef4263c430f740f26f9e3bc73fc025f6ea
Khvede93 <gio.khvede93@gmail.com>
2024-09-20 21:20:35 +0200

    temp testing

--- src/components/use-window-resize/Test.jsx
+++ src/components/use-window-resize/Test.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './styles.css';
+import useWindowResize from './useWindowResize';
 
 export const Test = () => {
-  return <div className='test'>Test</div>;
+  const windowSize = useWindowResize();
+  const { width, height } = windowSize;
+
+  return (
+    <div className='test'>
+      <h1>useWindowResize / useResponsive Custom Hook</h1>
+      <p>Width is: {width}</p>
+      <p>Height is: {height}</p>
+    </div>
+  );
 };
